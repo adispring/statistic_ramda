aa1ba3db0928ad0efbeca2eb794c193f478bd7e2
王增迪 <wangzengdi@meituan.com>
2017-10-22 00:35:41 +0800

    optimize code

--- ramda_frequence.sh
+++ ramda_frequence.sh
@@ -13,7 +13,9 @@ do
     cd "$PROJECTS_CONTAINER_DIR/$project_dir"
     is_git_project=$(git rev-parse --is-inside-work-tree 2>/dev/null)
     if [ -d "$PROJECTS_CONTAINER_DIR/$project_dir" -a "$is_git_project" ]; then
-        apis=$(git grep -n 'R\.[a-z_][a-zA-Z_]*' -- './server/*' './src/*' | grep -o 'R\.[a-zA-Z_]*' | grep -o '[a-zA-Z_]*$')
+        apis=$(git grep -n 'R\.[a-z_][a-zA-Z_]*' -- './server/*' './src/*' \
+                   | grep -o 'R\.[a-zA-Z_]*' \
+                   | grep -o '[a-zA-Z_]*$')
         for api in $apis
         do
             if [ -z ${ramdaapis[$api]} ]; then
@@ -34,6 +36,5 @@ sorted_keys=$(
         echo -e "| $api | ${ramdaapis[$api]} | ${percent}% |\n"
     done | sort -rn -k4)
 echo -e "$table_head\n$sorted_keys" | tee "$CURRENT_DIR/ramda-status.md"
-# echo "$sorted_keys" > "$CURRENT_DIR/ramda-status.md"
 
 
